d873eb6ba52c2ad00b876b4f2856872082829b7e
Pierre Lannoy <pierre@lannoy.fr>
2021-06-03 12:54:46 +0200

    Fixes vendor dir.

--- includes/libraries/class-libraries.php
+++ includes/libraries/class-libraries.php
@@ -83,7 +83,7 @@ class Libraries {
 		self::$psr4_libraries['psr-03']      = [
 			'name'    => 'PSR-3',
 			'prefix'  => 'Psr\\Log',
-			'base'    => IPLOCATOR_VENDOR_DIR . 'psr/log/',
+			'base'    => POSE_VENDOR_DIR . 'psr/log/',
 			'version' => '',
 			'author'  => 'PHP Framework Interop Group',
 			'url'     => 'https://www.php-fig.org/',
@@ -93,7 +93,7 @@ class Libraries {
 		self::$psr4_libraries['psr-07']      = [
 			'name'    => 'PSR-7',
 			'prefix'  => 'Psr\\Http\\Message',
-			'base'    => IPLOCATOR_VENDOR_DIR . 'psr/http-message/',
+			'base'    => POSE_VENDOR_DIR . 'psr/http-message/',
 			'version' => '',
 			'author'  => 'PHP Framework Interop Group',
 			'url'     => 'https://www.php-fig.org/',
@@ -103,7 +103,7 @@ class Libraries {
 		self::$psr4_libraries['psr-18']      = [
 			'name'    => 'PSR-18',
 			'prefix'  => 'Psr\\Http\\Client',
-			'base'    => IPLOCATOR_VENDOR_DIR . 'psr/http-client/',
+			'base'    => POSE_VENDOR_DIR . 'psr/http-client/',
 			'version' => '',
 			'author'  => 'PHP Framework Interop Group',
 			'url'     => 'https://www.php-fig.org/',
@@ -113,7 +113,7 @@ class Libraries {
 		self::$psr4_libraries['decalog-sdk'] = [
 			'name'    => 'DecaLog SDK',
 			'prefix'  => 'DecaLog',
-			'base'    => IPLOCATOR_VENDOR_DIR . 'decalog-sdk/',
+			'base'    => POSE_VENDOR_DIR . 'decalog-sdk/',
 			'version' => '1.0.0',
 			'author'  => 'Pierre Lannoy',
 			'url'     => 'https://github.com/Pierre-Lannoy/wp-decalog-sdk',
