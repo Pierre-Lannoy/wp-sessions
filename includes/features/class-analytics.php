364010a3952ddad3351ada0c2672f60cb8aa1fd8
Pierre Lannoy <pierre@lannoy.fr>
2021-11-26 23:18:41 +0100

    Improves X axis computation.

--- includes/features/class-analytics.php
+++ includes/features/class-analytics.php
@@ -470,14 +470,14 @@ class Analytics {
 			}
 
 			// Rendering.
-			if ( 4 < $this->duration ) {
-				if ( 1 === $this->duration % 2 ) {
-					$divisor = 6;
-				} else {
-					$divisor = 5;
+			$divisor = $this->duration + 1;
+			while ( 11 < $divisor ) {
+				foreach ( [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397 ] as $divider ) {
+					if ( 0 === $divisor % $divider ) {
+						$divisor = $divisor / $divider;
+						break;
+					}
 				}
-			} else {
-				$divisor = $this->duration + 1;
 			}
 			$result  = '<div class="pose-multichart-handler">';
 			$result .= '<div class="pose-multichart-item active" id="pose-chart-user">';
