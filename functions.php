e7c6d759af580fc0b887ceb7f242fb78da42a72c
Pierre Lannoy <pierre@hosterra.eu>
2024-05-28 08:32:47 +0200

    Adds Psr/Log version "detection".

--- functions.php
+++ functions.php
@@ -7,26 +7,35 @@
  * @since   2.3.0
  */
 
-/**
- * Provide PHP 7.3 compatibility for array_key_last() function.
- */
-if ( ! function_exists( 'array_key_last' ) ) {
-	// phpcs:ignore
-	function array_key_last( array $array ) {
-		if ( ! empty( $array ) ) {
-			return key( array_slice( $array, -1, 1, true ) );
+if ( ! function_exists('decalog_get_psr_log_version') ) {
+	/**
+	 * Get the needed version of PSR-3.
+	 *
+	 * @return  int  The PSR-3 needed version.
+	 * @since 4.0.0
+	 */
+	function decalog_get_psr_log_version() {
+		$required = 1;
+		if ( ! defined( 'DECALOG_PSR_LOG_VERSION') ) {
+			define( 'DECALOG_PSR_LOG_VERSION', 'V1' );
 		}
+		switch ( strtolower( DECALOG_PSR_LOG_VERSION ) ) {
+			case 'v3':
+				$required = 3;
+				break;
+			case 'auto':
+				if ( class_exists( '\Psr\Log\NullLogger') ) {
+					$reflection = new \ReflectionMethod(\Psr\Log\NullLogger::class, 'log');
+					foreach ( $reflection->getParameters() as $param ) {
+						if ( 'message' === $param->getName() ) {
+							if ( str_contains($param->getType() ?? '', '|') ) {
+								$required = 3;
+							}
+						}
+					}
+				}
+		}
+		return $required;
 	}
 }
 
-/**
- * Provide PHP 7.3 compatibility for array_key_first() function.
- */
-if ( ! function_exists( 'array_key_first' ) ) {
-	// phpcs:ignore
-	function array_key_first( array $arr ) {
-		foreach ( $arr as $key => $unused ) {
-			return $key;
-		}
-	}
-}
